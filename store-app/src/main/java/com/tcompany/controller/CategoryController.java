f1bd2183cca36c80cb9c5311429d88565fac571d
reinaldo <reinaldocostamendes3@gmail.com>
2022-12-19 14:35:13 +0000

    New app version

--- store-app/src/main/java/com/tcompany/controller/CategoryController.java
+++ store-app/src/main/java/com/tcompany/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.tcompany.controller;
 
+import com.tcompany.model.Category;
 import com.tcompany.model.Users;
+import com.tcompany.service.CategoryService;
 import com.tcompany.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,32 +16,24 @@ import java.io.IOException;
 //@Order(2)
 public class CategoryController {
 	@Autowired
-	private UserService userService;
+	private CategoryService categoryService;
 	@RequestMapping(method = RequestMethod.GET, value = { "/categories" })
 	public ModelAndView categories() {
 		ModelAndView andView = new ModelAndView("/categories/categories");
 
-		andView.addObject("users", userService.getAll());
-		andView.addObject("userobj", new Users());
+		andView.addObject("categories", categoryService.getAll());
+		andView.addObject("category", new Category());
 		
 		return andView;
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = { "/saveCategory", "*/saveCategory" })
-	public ModelAndView saveCategory(@ModelAttribute Users user)
+	public ModelAndView saveCategory(@ModelAttribute Category category)
 			 throws IOException {
-		
-		
-		ModelAndView andView =  new ModelAndView("/categories/categories");
-		if(user.getUser_id()>= 0 &&( user.getPassword()==null || user.getPassword().trim()=="")) {
-			Users user_tmp = userService.getUserById(user.getUser_id());
-			user.setPassword(user_tmp.getPassword());
-		}else{
-			user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-		}
-			Users user_saved = userService.addOrUpdateUSer(user);
-			andView.addObject("users", userService.getAll());
-			andView.addObject("userobj", user_saved);
+		ModelAndView andView = new ModelAndView("/categories/categories");
+			Category category_saved =categoryService.addOrUpdateCategory(category);
+			andView.addObject("categories", categoryService.getAll());
+			andView.addObject("category", category_saved);
 		andView.addObject("msg","Saved successful");
 	
 		return andView;
@@ -48,16 +42,16 @@ public class CategoryController {
 	public ModelAndView createCategory() {
 		ModelAndView andView = new ModelAndView("/categories/category-form");
 
-		andView.addObject("userobj", new Users());
-		andView.addObject("title", "Add New User");
+		andView.addObject("category", new Category());
+		andView.addObject("title", "Add New Category");
 		return andView;
 	}
 	@RequestMapping(method = RequestMethod.GET, value = "/listcategories")
 	public ModelAndView listCategories() {
 		ModelAndView andView = new ModelAndView("/categories/categories");
 
-		andView.addObject("users", userService.getAll());
-		andView.addObject("userobj", new Users());
+		andView.addObject("categories", categoryService.getAll());
+		andView.addObject("category", new Category());
 		
 		return andView;
 	}
@@ -65,25 +59,25 @@ public class CategoryController {
 	@GetMapping("/editCategory/{id}")
 	public ModelAndView editCategory(@PathVariable("id") Integer id) {
 		ModelAndView andView = new ModelAndView("/categories/category-form");
-		Users user = userService.getUserById(id);
-		andView.addObject("userobj", user);
-		andView.addObject("title", "Edit User");
+	Category category = categoryService.getCategoryById(id);
+		andView.addObject("category", category);
+		andView.addObject("title", "Edit Category");
 		return andView;
 	}
 
 	@RequestMapping(value = { "/deleteCategory/{id}" }, method = RequestMethod.GET)
 	public ModelAndView deleteCategory(@PathVariable("id") Integer id) throws Exception {
-	Users user =	userService.deleteUser(id);
+	Category category =	categoryService.deleteCategory(id);
 	String msg = "";
-	if(user==null){
-		msg="Not found user to delete";
+	if(category==null){
+		msg="Not found category to delete";
 	}else{
-		msg="User "+user.getFirs_name()+" deleted successful";
+		msg="Category "+category.getCategoryName()+" deleted successful";
 	}
-		ModelAndView andView = new ModelAndView("/users/users");
+		ModelAndView andView = new ModelAndView("/categories/categories");
 
-		andView.addObject("users", userService.getAll());
-		andView.addObject("userobj", new Users());
+		andView.addObject("categories", categoryService.getAll());
+		andView.addObject("category", new Category());
 		andView.addObject("msg",msg);
 		return andView;
 	}
